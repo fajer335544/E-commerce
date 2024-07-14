2d2b53577f3697cdd3011aaf9c0bbf342bb4fac3
Fejir <fajer335544@hotmail.com>
2024-07-14 11:04:51 +0300

    front end Added

---
+++ src/components/Image/Image.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './Image.css';
+
+const image = props => (
+  
+  <div
+    className="image"
+    style={{
+      backgroundImage: `url('${props.imageUrl}')`,
+      backgroundSize: props.contain ? 'contain' : 'cover',
+      backgroundPosition: props.left ? 'left' : 'center',
+    
+    }}
+  >
+<img src={props.imageUrl}alt="Preview"></img>
+  </div>
+ 
+);
+
+export default image;
